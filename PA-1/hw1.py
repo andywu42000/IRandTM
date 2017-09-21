b3effe701c2243937e42983ef336288ae3225811
jordanSu <newjordansu1126@gmail.com>
2017-09-22 01:24:02 +0800

    decode input and change the order of stemming with stop-words

--- PA-1/hw1.py
+++ PA-1/hw1.py
@@ -8,30 +8,30 @@ stop_words_list = []
 with open('stop-words.txt') as stop_data:
     stop_words_list = stop_data.read().split()
 
-regex = re.compile("[^a-zA-Z\-_]")
+regex = re.compile("[a-zA-Z\-_]+")
 
 with open('news.txt') as file:
     
     # Read the raw text and print it
-    raw_data = file.read()
+    raw_data = file.read().decode("utf-8")
 
     # Tokenization and Lowercase 
     token_list = []
     split_data = raw_data.lower().split()
     for s in split_data:
-        token_list.append(regex.sub('', s))
-    
-    # Stemming by using Porter's algorithm
-    stemmed_list = []
-    for s in token_list:
-        stemmed_list.append(p.stem(s, 0, len(s)-1))
+        token_list.append(regex.match(s).group())
 
     # Remove stop words
-    result_list = []
-    for s in stemmed_list:
+    after_removed_list = []
+    for s in token_list:
         if not s in stop_words_list:    
-            result_list.append(s)
+            after_removed_list.append(s)
     
+    # Stemming by using Porter's algorithm
+    result_list = []
+    for s in after_removed_list:
+        result_list.append(p.stem(s, 0, len(s)-1))
+
     # Write result to file
     file = open('result.txt','w')
     for result in result_list:
