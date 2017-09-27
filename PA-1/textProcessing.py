99fa9b061e0b56c0f6f1d7ff3d71ea8c3de1e39f
andywu42000 <andywu42000@users.noreply.github.com>
2017-09-27 23:20:30 +0800

    0927 Add comments

--- PA-1/textProcessing.py
+++ PA-1/textProcessing.py
@@ -1,16 +1,37 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[10]:
 
-inputFile = open('news.txt', 'r', encoding='UTF-8')
-outputFile = open('result.txt','w', encoding='UTF-8')
+import re, nltk
+from nltk.stem.porter import *
 
-for line in inputFile:     
-    outputFile.write(line.lower())
-    
-inputFile.close()
-outputFile.close()
+# R0672532
+# 開啟news.txt檔並作初步的分割、去跳行及全小寫化
+raw = open('news.txt', 'r', encoding='UTF-8').read().lower().strip().split()
+
+# token化分割的單詞
+tokens = []
+for text in raw:
+    tokens.append(re.compile("[^a-zA-Z\-_]").sub('', text))
+
+# 使用nltk的PorterStemmer
+stemmer = PorterStemmer()
+singles = [stemmer.stem(token) for token in tokens]
+
+# 開啟stopwords.txt去除stopwords並輸出結果
+stopwords = open('stopwords.txt', 'r', encoding='UTF-8').read()
+filtered_words = [word for word in singles if word not in stopwords]
+
+output_file = open('result.txt', 'w', encoding='UTF-8')
+for word in filtered_words:
+    output_file.write(word + '\n')
+output_file.close()
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
