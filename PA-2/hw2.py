9458ff9363e12e0f43a30395627227463d5fc61b
sharonWU0505 <astrumstella048@gmail.com>
2017-10-22 18:38:43 +0800

    Final revision and fix bugs

--- PA-2/hw2.py
+++ PA-2/hw2.py
@@ -1,12 +1,14 @@
 # !/usr/local/bin/python3
 # -*- coding: utf-8 -*-
+import math
 import os
 from process_content import (
     clear_content, stem, generate_term_list)
 
 
 DOCUMENT_DIRECTORY = "documents/"
-DICTIONARY_FILEPATH = "result/dictionary.txt"
+DICTIONARY_FILEPATH = "dictionary.txt"
+RESULT_DIRECTORY = "result/"
 
 
 def prepare_content(directory):
@@ -23,17 +25,71 @@ def prepare_content(directory):
     return cleared_content
 
 
-def generate_dictionary(term_list, content_list):
-    """Generate df value for each term."""
-    term_df_list = []
-    for index, term in enumerate(term_list, start=1):
-        df = 0
-        for content in content_list:
-            if term in content:
-                df += 1
-        term_df_list.append((index, term, df))
+def generate_dictionary(term_list, doc_list):
+    """
+    Generate df and tf value for each term and each doc.
+    dictionary = [
+        {
+            df: df-value,
+            tf: {
+                doc_id: tf-value, ...
+            }
+        }, ...
+    ]
+    """
+    # initialize
+    dictionary = [
+        {
+            "df": 0,
+            "tf": {}
+        }
+        for term in term_list
+    ]
+    terms_for_doc = [[] for doc in doc_list]
 
-    return term_df_list
+    # generate df and tf value
+    for term_index, term in enumerate(term_list):
+        for doc_index, doc in enumerate(doc_list):
+            if term in doc:
+                dictionary[term_index]["df"] += 1
+                terms_for_doc[doc_index].append(term_index)
+                tf_value = doc.count(term)
+                dictionary[term_index]["tf"][str(doc_index)] = tf_value
+
+    return dictionary, terms_for_doc
+
+
+def get_tf_related_values_and_write_file(dictionary,
+                                         terms_for_doc,
+                                         idf_list,
+                                         doc_index=0):
+    # get tf values
+    term_index_list = terms_for_doc[doc_index]
+    tf_list = [
+        dictionary[term_index]["tf"][str(doc_index)]
+        for term_index in term_index_list
+    ]
+
+    # process tf-idf values
+    tf_idf_list = []
+    for term_index, tf in enumerate(tf_list):
+        tf_idf = float(tf * idf_list[term_index_list[term_index]])
+        tf_idf_list.append(tf_idf)
+
+    # process normalized tf-idf value and write file
+    output_filepath = "Doc{id}.txt".format(id=(doc_index + 1))
+    doc_file = open(RESULT_DIRECTORY + output_filepath, "w", encoding="utf-8")
+    doc_file.write("{term_count}\n".format(
+        term_count=str(len(term_index_list))))
+
+    vector_length = math.sqrt(sum([value**2 for value in tf_idf_list]))
+    for index, tf_idf in enumerate(tf_idf_list):
+        doc_file.write(
+            '{id:<10}{ntf_idf:<30}\n'.format(
+                id=str(term_index_list[index]),
+                ntf_idf=str(tf_idf / vector_length))
+        )
+    doc_file.close()
 
 
 # Generate term list
@@ -44,12 +100,25 @@ term_list = generate_term_list(" ".join(cleared_content))
 processed_content = []
 for content in cleared_content:
     content = content.split(" ")
-    processed_content.append(" ".join(stem(content)))
-term_df_list = generate_dictionary(term_list, processed_content)
+    processed_content.append(stem(content))
+doc_count = len(processed_content)
+dictionary, terms_for_doc = generate_dictionary(
+    term_list, processed_content)
 
 # Write dictionary.txt
 dict_file = open(DICTIONARY_FILEPATH, "w", encoding="utf-8")
-for term_df in term_df_list:
+for term_index, term in enumerate(dictionary):
     dict_file.write(
-        '{0[0]:<10}{0[1]:<30}{0[2]:<10}\n'.format(term_df))
+        '{id:<10}{term:<30}{df:<10}\n'.format(
+            id=str(term_index + 1),
+            term=term_list[term_index],
+            df=str(term["df"]))
+    )
 dict_file.close()
+
+# Get idf list
+idf_list = [math.log10(doc_count / term["df"]) for term in dictionary]
+
+# Get n_tf_idf_value and write file
+get_tf_related_values_and_write_file(
+    dictionary, terms_for_doc, idf_list)
