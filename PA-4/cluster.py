4b4aad236ddc89d3eb18ee4ce6e37fb1b9d1cd57
sharonWU0505 <astrumstella048@gmail.com>
2018-01-08 08:59:22 +0800

    Add cluster as a class

---
+++ PA-4/cluster.py
@@ -0,0 +1,26 @@
+from priority_queue import Heap
+import numpy as np
+
+
+class Cluster:
+    def __init__(self, doc, ini_sim_list):
+        self.docs = [doc]
+        self.size = 1
+        self.centroid = np.array(doc)
+        self.pri_queue = Heap()
+        self.pri_queue.build_heap(ini_sim_list)
+
+    def merge(self, new_cluster):
+        self.docs = self.docs.extend(new_cluster.docs)
+        print(self.docs)
+
+        n1 = self.size
+        n2 = new_cluster.size
+        self.centroid = (n1 * self.centroid + n2 * new_cluster.centroid) / (n1 + n2)
+
+        self.size += new_cluster.size
+
+    def get_most_sim(self):
+        sim_value = self.pri_queue.heapList[1][1]
+        sim_idx = self.pri_queue.heapList[1][0]
+        return sim_value, sim_idx
